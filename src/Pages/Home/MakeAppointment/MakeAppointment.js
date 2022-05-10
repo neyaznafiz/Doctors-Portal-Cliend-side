960370bf58ef60ee7b584c7f6935794831efef35
niiyazobug <niiyaz5555@gmail.com>
2022-05-10 21:04:33 +0600

    makeAppointment secton done

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -8,17 +8,17 @@ const MakeAppointment = () => {
         <section style={{
             background: `url(${appointment})`
         }}
-            className='flex justify-center items-center'>
+            className='flex justify-center items-center my-48'>
 
-            <div className='flex-1'>
-                <img src={doctor} alt="" />
+            <div className='flex-1 hidden lg:block'>
+                <img src={doctor} alt="" className='-mt-[200px]' />
             </div>
             <div className='flex-1'>
-                <h3 className='text-xl text-primary'>Appointment</h3>
+                <h3 className='text-xl text-primary font-bold'>Appointment</h3>
 
                 <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
 
-                <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dicta esse quisquam illo ratione dolorem labore. Aliquid, eos. Voluptatibus eligendi quidem deserunt sint aut dolorum, est obcaecati exercitationem? Vitae blanditiis impedit officia quam doloribus repudiandae minus provident velit incidunt vero?</p>
+                <p className='text-white my-4 pr-0 lg:pr-60'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
 
                 <PrimaryButton>get started</PrimaryButton>
             </div>
