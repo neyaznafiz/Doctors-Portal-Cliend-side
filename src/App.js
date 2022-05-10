3aa4c21fa85a4731d5f773ce000c833100562c70
niiyazobug <niiyaz5555@gmail.com>
2022-05-10 12:19:44 +0600

    route created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import Home from './Pages/Home/Home';
+import Reviews from './Pages/Reviews/Reviews';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-  <Navbar></Navbar>
+      <Navbar></Navbar>
+
+      <Routes>
+
+<Route path='/' element={<Home></Home>} />
+<Route path='/about' element={<About></About>} />
+<Route path='/appointment' element={<Appointment></Appointment>} />
+<Route path='/reviews' element={<Reviews></Reviews>} />
+
+      </Routes>
     </div>
   );
 }
