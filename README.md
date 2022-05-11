f383bf2f4c66a5516f321c3aac16526515aeea77
niiyazobug <niiyaz5555@gmail.com>
2022-05-11 20:43:39 +0600

    server link added in availableAppointment data fetch]

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Live side Link 
 
 ## https://doctors-protal-n.web.app/
+
+# Server repo Link
+
+## https://github.com/neyaznafiz/Doctors-Portal-server-side
--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -9,16 +9,16 @@ const AvailableAppointments = ({ date }) => {
     const [treatment, setTreatment] = useState(null)
 
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
 
     return (
         <div>
-            <h4 className='text-xl text-secondary text-center'>Available Appointments on  {format(date, 'PP')}</h4>
+            <h4 className='text-xl text-secondary text-center pt-10'>Available Appointments on  {format(date, 'PP')}</h4>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-12'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-12 py-24'>
                 {
                     services.map(service => <Service
                     key={service._id}
