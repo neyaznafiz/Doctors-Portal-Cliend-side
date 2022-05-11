bcad93a7347021279ccddf0c08f9d3ab74e57657
niiyazobug <niiyaz5555@gmail.com>
2022-05-11 12:50:28 +0600

    services data fetch

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,10 +1,23 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const AvailableAppointments = ({date}) => {
+const AvailableAppointments = ({ date }) => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'>Available Appointments on  {format(date, 'PP')}</h4>
+
+            <div>
+                
+            </div>
         </div>
     );
 };
