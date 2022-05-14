88ac73806ad473cf6e4f9fcc8cc2465a0b33fc7d
niiyazobug <niiyaz5555@gmail.com>
2022-05-14 11:35:17 +0600

    display service without that time bokked

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -8,8 +8,9 @@ const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([])
     const [treatment, setTreatment] = useState(null)
 
+    const formatedDate = format(date, 'PPPP')
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formatedDate}`)
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
