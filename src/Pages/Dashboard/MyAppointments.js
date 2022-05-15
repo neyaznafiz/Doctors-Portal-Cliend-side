1a234a33004c03adece38f624b6655d7610b6655
niiyazobug <niiyaz5555@gmail.com>
2022-05-15 15:17:41 +0600

    if token not match to a user throw error and navigate home

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../FIrebase/firebase.init';
 
 const MyAppointments = () => {
@@ -7,16 +8,26 @@ const MyAppointments = () => {
     const [appointments, setAppointments] = useState([])
     const [user] = useAuthState(auth);
 
+    const Navigate = useNavigate()
+
     useEffect(() => {
         if (user) {
             fetch(`http://localhost:5000/booking?patient=${user.email}`, {
                 method: 'GET',
                 headers: {
-                    'authorization': `bearer ${ localStorage.getItem('accessToken') }`
+                    'authorization': `bearer ${localStorage.getItem('accessToken')}`
                 }
             })
-                .then(res => res.json())
-                .then(data => setAppointments(data))
+                .then(res => {
+                    if (res.status === 401 || res.status === 403) {
+                        Navigate('/')
+                    }
+                    return res.json()
+                })
+                .then(data => {
+
+                    setAppointments(data)
+                })
         }
     }, [user])
 
