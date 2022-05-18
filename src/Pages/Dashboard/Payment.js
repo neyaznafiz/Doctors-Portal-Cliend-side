5f0e78f2e3c647648d65402a9ad51972b8c06f95
niiyazobug <niiyaz5555@gmail.com>
2022-05-18 12:20:52 +0600

    show service inf in payment

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
 
 const Payment = () => {
 
-    const {id}=useParams()
+    const { id } = useParams()
+
+    const url = `http://localhost:5000/booking/${id}`
+
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
     return (
-        <div>
-            <h2 className="text-2xl text-zinc-800">Please pay for: {id}</h2>
+        <div className='grid justify-center'>
+
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className="text-success text-xl font-semibold">Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Pay for {appointment.treatment}</h2>
+                    <p> Your Appointment: <span className='font-semibold'>{appointment.date}</span> at <span className='font-semibold'>{appointment.slot}</span></p>
+                  <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
+
+                </div>
+            </div>
+
 
         </div>
     );
